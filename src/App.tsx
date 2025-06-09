06a43ec2df591f5be1c55d9a5bdca3125fd73a54
enzoosorio <136608992+enzoosorio@users.noreply.github.com>
2025-06-09 00:48:22 -0500

    build issue :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { MainCard } from './components/Hero/parts/first-part/main-card'
-
 function App() {
   
 
   return (
     <main className='h-[300dvh] relative w-full bg-white'>
-      <MainCard />
     </main>
   )
 }
